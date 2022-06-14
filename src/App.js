969e5baa7d1fc58584c954138c0398a8840dc158
TarunKumarP <tarun.kumar@thinkjs.co>
2022-06-14 13:05:22 +0530

    ArrayMethods( pop,push,shift,unshift)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ArrayMethods from './JavaScript/ArrayMethods';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+    <ArrayMethods />
     </div>
   );
 }
---
+++ src/JavaScript/ArrayMethods.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ArrayMethods = () => {
+    // let array = ["Ntr","Vijay","Arjun","Ajith"]
+    // array.pop means delete the last element in array
+    // let popped = array.pop()
+    // console.log(array);
+    let array1 = ["Chandu","Sai","Sindhu","Charan"]
+    //arrayy.push means adds an element at the end of an array
+    // let pushed = array1.push("Srinu");
+    //arrayy.shift means delete the first element of an arrayy
+    // let shifted = array1.shift()
+    // arrayy.unshift("") means adds an element at first of an array.
+    // let unshifted = array1.unshift(2,"Emanuel")
+    console.log(array1)
+  return (
+    <div>
+      Hello 
+    </div>
+  )
+}
+
+export default ArrayMethods
